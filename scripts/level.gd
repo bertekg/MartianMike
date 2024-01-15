599d0a7b8b561f89a93ac9672ab5db8e4cf54c5b
bertekg <bartek.grywalski@gmail.com>
2024-01-15 20:02:58 +0100

    L67 The For Loop

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,17 +4,10 @@ extends Node2D
 @onready var player = $Player
 
 func _ready():
-	#var array = [1, 2, 3, "hello", 2.3, true]
-	#array[0] = "first"
-	#array.append("new append")
-	#array.remove_at(1)
-	#array.erase("hello")
-	#print(array)
-	#print(array[1])
-	#print(array[-2])
-	#print("Size of array: " + str(array.size()))
-	var traps = get_tree().get_nodes_in_group("traps")
-	print(traps.size())
+	var traps = get_tree().get_nodes_in_group("traps")	
+	for trap in traps:
+		trap.connect("touchedPlayer", _on_trap_touched_player)
+		#trap.touchedPlayer.connect(_on_trap_touched_player)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
